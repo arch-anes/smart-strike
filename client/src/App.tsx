471c87d58c433b5307e826cb19d8aad7953db811
Anes Belfodil <ans.belfodil@gmail.com>
2022-04-23 16:58:07 +0000

    Format

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,52 +13,52 @@ const redirectUrl = window.location.origin + window.location.pathname;
 const authorizationUrl = `https://${authFQDN}/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&response_type=token&state=STATE`;
 
 if (idToken === null) {
-  window.location.href = authorizationUrl;
+    window.location.href = authorizationUrl;
 }
 
 window.location.hash = '';
 
 const tryOpenDoor = async () =>
-  axios
-    .post(
-      'https://tgfue4mzd5.execute-api.us-east-1.amazonaws.com/smart-strike-api/strike',
-      {},
-      {
-        headers: {
-          Authorization: idToken as string,
-        },
-      },
-    )
-    .then(() => true)
-    .catch((response) => {
-      console.error(response);
-      return false;
-    });
+    axios
+        .post(
+            'https://tgfue4mzd5.execute-api.us-east-1.amazonaws.com/smart-strike-api/strike',
+            {},
+            {
+                headers: {
+                    Authorization: idToken as string,
+                },
+            }
+        )
+        .then(() => true)
+        .catch((response) => {
+            console.error(response);
+            return false;
+        });
 
 const App = () => {
-  const [active, setActive] = useState(false);
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
+    const [active, setActive] = useState(false);
+    const timerRef = useRef<NodeJS.Timeout | null>(null);
 
-  const handleClick = () =>
-    tryOpenDoor().then((success) => {
-      if (!success) return;
-      setActive(true);
-      timerRef.current && clearTimeout(timerRef.current);
-      timerRef.current = setTimeout(() => {
-        timerRef.current = null;
-        setActive(false);
-      }, 5000);
-    });
+    const handleClick = () =>
+        tryOpenDoor().then((success) => {
+            if (!success) return;
+            setActive(true);
+            timerRef.current && clearTimeout(timerRef.current);
+            timerRef.current = setTimeout(() => {
+                timerRef.current = null;
+                setActive(false);
+            }, 5000);
+        });
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Button variant={active ? 'primary' : 'secondary'} size="lg">
-          <FontAwesomeIcon icon={faDoorOpen} onClick={handleClick} />
-        </Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button variant={active ? 'primary' : 'secondary'} size="lg">
+                    <FontAwesomeIcon icon={faDoorOpen} onClick={handleClick} />
+                </Button>
+            </header>
+        </div>
+    );
 };
 
 export default App;
